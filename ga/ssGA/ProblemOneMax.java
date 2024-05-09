9af1649c1195c10a3f8e9c40062ce3d3f81aff29
Miguel Játiva <migueljativa4@gmail.com>
2024-05-09 18:03:08 +0200

    Reformat code
    Reformat code using: https://github.com/google/google-java-format

--- ga/ssGA/ProblemOneMax.java
+++ ga/ssGA/ProblemOneMax.java
@@ -7,33 +7,27 @@
 package ga.ssGA;
 
 /**
- *
- * @author  Antonio
- * @version 
+ * @author Antonio
+ * @version
  */
-public class ProblemOneMax extends Problem{
+public class ProblemOneMax extends Problem {
 
-    /** Creates new ProblemOneMax */
-    //public ProblemOneMax() {
-    //    super() ;
-    //}
+  /** Creates new ProblemOneMax */
+  // public ProblemOneMax() {
+  //    super() ;
+  // }
 
-    public double Evaluate(Individual Indiv) {
-      return ONEMAX(Indiv) ;
-    }
-
+  public double Evaluate(Individual Indiv) {
+    return ONEMAX(Indiv);
+  }
 
   //    PRIVATE METHODS
 
   // Count the number of 1's in the string
-  private double ONEMAX(Individual indiv)
-  {
-    double f=0.0;
-    for(int i=0; i<CL; i++)
-    if(indiv.get_allele(i)==1)
-    f=f+1.0;
+  private double ONEMAX(Individual indiv) {
+    double f = 0.0;
+    for (int i = 0; i < CL; i++) if (indiv.get_allele(i) == 1) f = f + 1.0;
     indiv.set_fitness(f);
     return f;
   }
-
 }
